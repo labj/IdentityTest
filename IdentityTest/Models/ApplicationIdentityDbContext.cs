eb984c30cd1abb7abb087a38f611fe861aec8d69
Lars Björklund <38240941+labj@users.noreply.github.com>
2020-11-04 17:43:56 +0100

    Add project files.

---
+++ IdentityTest/Models/ApplicationIdentityDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityTest.Models
+{
+    public class ApplicationIdentityDbContext : Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationIdentityDbContext(DbContextOptions<ApplicationIdentityDbContext> options) 
+            : base(options) 
+        { 
+        }
+    }
+}
